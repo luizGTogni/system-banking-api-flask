bae9a19f3a3f0fa613c3a413b08ae36c941ea72f
Luiz Togni <contato.togni@gmail.com>
2025-08-02 20:02:39 -0300

    feat: add customer interface model

--- src/models/interfaces/customer.py
+++ src/models/interfaces/customer.py
@@ -3,9 +3,25 @@ from abc import ABC, abstractmethod
 class CustomerInterface(ABC):
 
     @abstractmethod
-    def generate_report(self) -> None:
+    def create(
+        self,
+        name: str,
+        age: int,
+        phone: str,
+        email: str,
+        category: str,
+        revenue: float
+    ) -> None:
         pass
 
     @abstractmethod
-    def withdraw(self, value: float) -> None:
+    def list_all(self) -> list:
+        pass
+
+    @abstractmethod
+    def generate_report(self, customer_id: int) -> dict:
+        pass
+
+    @abstractmethod
+    def withdraw(self, customer_id: int, value: float) -> bool:
         pass
