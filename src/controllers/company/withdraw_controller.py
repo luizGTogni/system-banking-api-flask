5837fe67f9849c57f2c71d4c5d4c0d499d6bae39
Luiz Togni <contato.togni@gmail.com>
2025-08-03 01:47:51 -0300

    feat: withdraw controller

---
+++ src/controllers/company/withdraw_controller.py
@@ -0,0 +1,34 @@
+from src.models.interfaces.customer import CustomerInterface
+from src.controllers.interfaces.withdraw_controller import CustomerWithdrawControllerInterface
+
+class CompanyWithdrawController(CustomerWithdrawControllerInterface):
+    def __init__(self, company_repository: CustomerInterface) -> None:
+        self.__company_repository = company_repository
+
+    def withdraw(self, customer_id: int, value: float) -> dict:
+        self.__validate_value(value)
+        is_valid_withdraw = self.__decrease_balance_in_db(customer_id, value)
+        response = self.__format_response(is_valid_withdraw)
+
+        return response
+
+    def __validate_value(self, value: float) -> None:
+        if not isinstance(value, (float, int)):
+            raise Exception("value not number")
+
+        if value <= 0:
+            raise Exception("value not is valid")
+
+    def __decrease_balance_in_db(self, customer_id: int, value: float) -> bool:
+        return self.__company_repository.withdraw(customer_id, value)
+
+    def __format_response(self, is_valid_withdraw: bool) -> dict:
+        return {
+            "data": {
+                "type": "Company",
+                "count": 1,
+                "attributes": {
+                    "valid": is_valid_withdraw
+                }
+            }
+        }
