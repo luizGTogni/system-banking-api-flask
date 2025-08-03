aa8dd7f5616f73132bb980734011a97efa180c19
Luiz Togni <contato.togni@gmail.com>
2025-08-03 16:38:32 -0300

    feat: add error handler with http response custom

--- src/main/routes/companies_routes.py
+++ src/main/routes/companies_routes.py
@@ -1,40 +1,58 @@
+# pylint: disable=broad-exception-caught
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.main.composer.company.create_view_composer import company_create_view_composer
 from src.main.composer.company.list_all_view_composer import company_list_all_view_composer
 from src.main.composer.company.report_view_composer import company_gen_report_view_composer
 from src.main.composer.company.withdraw_view_composer import company_withdraw_view_composer
+from src.main.errors.error_handler import error_handler
 
 company_route_bp = Blueprint("companies_routes", __name__)
 
 @company_route_bp.route("/companies", methods=["POST"])
 def create_customer():
-    http_request = HttpRequest(body=request.json)
-    view = company_create_view_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest(body=request.json)
+        view = company_create_view_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = error_handler(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @company_route_bp.route("/companies", methods=["GET"])
 def list_all_customers():
-    http_request = HttpRequest()
-    view = company_list_all_view_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest()
+        view = company_list_all_view_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+            http_response = error_handler(exception)
+            return jsonify(http_response.body), http_response.status_code
 
 @company_route_bp.route("/companies/<customer_id>/report", methods=["GET"])
 def generate_report(customer_id):
-    http_request = HttpRequest(param={ "customer_id": customer_id })
-    view = company_gen_report_view_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest(param={ "customer_id": customer_id })
+        view = company_gen_report_view_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = error_handler(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @company_route_bp.route("/companies/<customer_id>/withdraw", methods=["POST"])
 def withdraw(customer_id):
-    http_request = HttpRequest(body=request.json, param={ "customer_id": customer_id })
-    view = company_withdraw_view_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest(body=request.json, param={ "customer_id": customer_id })
+        view = company_withdraw_view_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = error_handler(exception)
+        return jsonify(http_response.body), http_response.status_code
--- src/main/routes/individuals_routes.py
+++ src/main/routes/individuals_routes.py
@@ -1,40 +1,58 @@
+# pylint: disable=broad-exception-caught
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.main.composer.individual.create_view_composer import individual_create_view_composer
 from src.main.composer.individual.list_all_view_composer import individual_list_all_view_composer
 from src.main.composer.individual.report_view_composer import individual_gen_report_view_composer
 from src.main.composer.individual.withdraw_view_composer import individual_withdraw_view_composer
+from src.main.errors.error_handler import error_handler
 
 individual_route_bp = Blueprint("individuals_routes", __name__)
 
 @individual_route_bp.route("/individuals", methods=["POST"])
 def create_customer():
-    http_request = HttpRequest(body=request.json)
-    view = individual_create_view_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest(body=request.json)
+        view = individual_create_view_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = error_handler(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @individual_route_bp.route("/individuals", methods=["GET"])
 def list_all_customers():
-    http_request = HttpRequest()
-    view = individual_list_all_view_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest()
+        view = individual_list_all_view_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = error_handler(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @individual_route_bp.route("/individuals/<customer_id>/report", methods=["GET"])
 def generate_report(customer_id):
-    http_request = HttpRequest(param={ "customer_id": customer_id })
-    view = individual_gen_report_view_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest(param={ "customer_id": customer_id })
+        view = individual_gen_report_view_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = error_handler(exception)
+        return jsonify(http_response.body), http_response.status_code
 
 @individual_route_bp.route("/individuals/<customer_id>/withdraw", methods=["POST"])
 def withdraw(customer_id):
-    http_request = HttpRequest(body=request.json, param={ "customer_id": customer_id })
-    view = individual_withdraw_view_composer()
-    http_response = view.handle(http_request)
+    try:
+        http_request = HttpRequest(body=request.json, param={ "customer_id": customer_id })
+        view = individual_withdraw_view_composer()
+        http_response = view.handle(http_request)
 
-    return jsonify(http_response.body), http_response.status_code
+        return jsonify(http_response.body), http_response.status_code
+    except Exception as exception:
+        http_response = error_handler(exception)
+        return jsonify(http_response.body), http_response.status_code
