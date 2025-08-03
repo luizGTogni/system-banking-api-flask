d02f056755435cc4ac456a1242afd0667a333c80
Luiz Togni <contato.togni@gmail.com>
2025-08-03 16:36:15 -0300

    refactor: add validators http request body

--- src/views/create_view.py
+++ src/views/create_view.py
@@ -2,12 +2,15 @@ from src.views.interfaces.view_interface import ViewInterface
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 from src.controllers.interfaces.create_controller import CustomerCreateControllerInterface
+from src.main.validators.create_customer_validator import create_customer_validator
 
 class CreateView(ViewInterface):
     def __init__(self, controller: CustomerCreateControllerInterface) -> None:
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
+        create_customer_validator(http_request)
+
         customer_info = http_request.body
         body_response = self.__controller.create(customer_info)
 
