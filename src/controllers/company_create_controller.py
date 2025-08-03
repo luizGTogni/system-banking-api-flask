1f08e8b6d66cf1a26d18c67d520e32d826f230b9
Luiz Togni <contato.togni@gmail.com>
2025-08-03 00:19:08 -0300

    feat: add company create controller

---
+++ src/controllers/company_create_controller.py
@@ -0,0 +1,46 @@
+from src.models.interfaces.customer import CustomerInterface
+from src.controllers.interfaces.customer_create_controller import CustomerCreateControllerInterface
+
+class CompanyCreateController(CustomerCreateControllerInterface):
+    def __init__(self, company_repository: CustomerInterface) -> None:
+        self.__company_repository = company_repository
+
+    def create(self, customer_info: dict) -> dict:
+        name = customer_info["name"]
+        age = customer_info["age"]
+        phone = customer_info["phone"]
+        email = customer_info["email"]
+        category = customer_info["category"]
+        monthly_income = customer_info["monthly_income"]
+
+        self.__insert_customer_in_db(name, age, phone, email, category, monthly_income)
+        response = self.__format_response(customer_info)
+
+        return response
+
+    def __insert_customer_in_db(
+            self,
+            name: str,
+            age: int,
+            phone: str,
+            email: str,
+            category: str,
+            monthly_income: float
+    ) -> None:
+        self.__company_repository.create(
+            name=name,
+            age=age,
+            phone=phone,
+            email=email,
+            category=category,
+            monthly_income=monthly_income
+        )
+
+    def __format_response(self, customer_info: dict) -> dict:
+        return {
+            "data": {
+                "type": "Company",
+                "count": 1,
+                "attributes": customer_info,
+            }
+        }
