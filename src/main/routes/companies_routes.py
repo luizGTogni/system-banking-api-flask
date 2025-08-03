318f57d8faf14160585e57ad3363b07164cc606e
Luiz Togni <contato.togni@gmail.com>
2025-08-03 12:33:00 -0300

    feat: add list all companies route

--- src/main/routes/companies_routes.py
+++ src/main/routes/companies_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.main.composer.company.create_view_composer import company_create_view_composer
+from src.main.composer.company.list_all_view_composer import company_list_all_view_composer
 
 company_route_bp = Blueprint("companies_routes", __name__)
 
@@ -8,6 +9,14 @@ company_route_bp = Blueprint("companies_routes", __name__)
 def create_customer():
     http_request = HttpRequest(body=request.json)
     view = company_create_view_composer()
+    http_response = view.handle(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
+
+@company_route_bp.route("/companies", methods=["GET"])
+def list_all_customers():
+    http_request = HttpRequest()
+    view = company_list_all_view_composer()
     http_response = view.handle(http_request)
 
     return jsonify(http_response.body), http_response.status_code
