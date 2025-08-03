fb771a28661a45fd534f3f195db4601ef18d7136
Luiz Togni <contato.togni@gmail.com>
2025-08-03 01:39:38 -0300

    refactor: change name

--- src/controllers/interfaces/customer_create_controller.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class CustomerCreateControllerInterface(ABC):
-
-    @abstractmethod
-    def create(self, customer_info: dict) -> dict:
-        pass
--- src/controllers/interfaces/customer_list_all_controller.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class CustomerListAllControllerInterface(ABC):
-
-    @abstractmethod
-    def list_all(self) -> dict:
-        pass
