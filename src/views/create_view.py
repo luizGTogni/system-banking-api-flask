c74fe00bf566b0abd6379dfb6213a5d9da17caf2
Luiz Togni <contato.togni@gmail.com>
2025-08-03 11:17:32 -0300

    feat: add create customer view

---
+++ src/views/create_view.py
@@ -0,0 +1,14 @@
+from src.views.interfaces.view_interface import ViewInterface
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+from src.controllers.interfaces.create_controller import CustomerCreateControllerInterface
+
+class CreateView(ViewInterface):
+    def __init__(self, controller: CustomerCreateControllerInterface) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        customer_info = http_request.body
+        body_response = self.__controller.create(customer_info)
+
+        return HttpResponse(status_code=201, body=body_response)
