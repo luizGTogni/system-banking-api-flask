47a0e13e2ab3e059679473da62ce77057db4538c
Luiz Togni <contato.togni@gmail.com>
2025-08-03 01:48:03 -0300

    test: add withdraw controller test

---
+++ src/controllers/company/withdraw_controller_test.py
@@ -0,0 +1,39 @@
+from pytest import raises
+from src.controllers.mocks.mock_repository import MockCustomerRepository
+from .withdraw_controller import CompanyWithdrawController
+
+def test_withdraw():
+    mock_repository = MockCustomerRepository()
+    controller = CompanyWithdrawController(company_repository=mock_repository)
+    response = controller.withdraw(customer_id=10, value=100)
+
+
+    assert response == {
+        "data": {
+            "type": "Company",
+            "count": 1,
+            "attributes": {
+                "valid": True
+            }
+        }
+    }
+
+def test_withdraw_validate_value_not_number():
+    mock_repository = MockCustomerRepository()
+    controller = CompanyWithdrawController(company_repository=mock_repository)
+
+    with raises(Exception) as excinfo:
+        controller.withdraw(customer_id=10, value="A")
+
+
+    assert str(excinfo.value) == "value not number"
+
+def test_withdraw_validate_value_not_is_valid():
+    mock_repository = MockCustomerRepository()
+    controller = CompanyWithdrawController(company_repository=mock_repository)
+
+    with raises(Exception) as excinfo:
+        controller.withdraw(customer_id=10, value=-1)
+
+
+    assert str(excinfo.value) == "value not is valid"
