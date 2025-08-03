53ad1572c06b87f30173f16175cde918e025306e
Luiz Togni <contato.togni@gmail.com>
2025-08-02 23:36:11 -0300

    feat: add mock customer repository

---
+++ src/controllers/mocks/mock_repository.py
@@ -0,0 +1,11 @@
+class MockCustomerRepository:
+    def create(
+        self,
+        name: str,
+        age: int,
+        phone: str,
+        email: str,
+        category: str,
+        monthly_income: float
+    ) -> None:
+        pass
